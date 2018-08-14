c74a922e28aae528eb3252bbe98a14d011e2fbc0
Quang Huy <dqhuy78@gmail.com>
2018-08-14 14:20:52 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SignupForm from './components/Forms/SignupForm';
+import SignupForm from './components/Forms/DemoForm';
 
 class App extends Component {
     render() {
